c0e20ac74205805d0f91430b1cd4954f0b173bd0
Thurzinfs <francasilvaarthur95@gmail.com>
2026-05-12 22:17:28 -0300

    feat: oficializando o gerenciador de deteccao do isolamento de pessoas

---
+++ app/managers/isolation_detector.py
@@ -0,0 +1,38 @@
+from numpy import array
+from sklearn.cluster import DBSCAN
+
+
+class DBScanManager:
+    def __init__(self, eps: float = float(250)) -> None:
+        self.scan = DBSCAN(eps=eps, min_samples=2)
+        self.inf_persons: dict 
+        self.persons_cord: list
+
+    def _to_list(self, inf: dict):
+        return [p['box'] for p in inf.values()]
+
+    def _to_array(self, model: list):
+        return array(model)
+
+    def find_by_lonely_in_peoples(self, predictions):
+
+        keys = list(self.inf_persons.keys())
+
+        lonelys = []
+        for i, value in enumerate(predictions):
+            if value == -1:
+                lonelys.append(keys[i])
+
+        return lonelys
+
+    def execute(self, persons):
+        boxes = self._to_list(persons)
+
+        self.persons_cord = boxes
+        self.inf_persons = persons
+
+        boxes = self._to_array(boxes)
+
+        clusters = self.scan.fit_predict(boxes)
+
+        return self.find_by_lonely_in_peoples(clusters)
